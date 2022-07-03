cdab1a0fb0f84368e65e77173aef8fb476b20fa3
Ignacio Iglesias <ignacioniglesias@gmail.com>
2022-07-03 00:33:59 -0300

    Estilo y relleno para Novedades

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,10 +1,19 @@
+import '../styles/components/pages/NovedadesPage.css'
 const NovedadesPage = (props) => {
     return (
         <section className="holder">
             <h2>Novedades</h2>
-            <h3>Titulo</h3>
-            <h4>Subtitulo</h4>
-            <p>Cuerpo</p>
+            <div className="amogus">
+            <h3>Amongus Cautivo</h3>
+            <img id="imagenAmogus"src="/images/novedades.png" alt="amogus"></img>
+            <h4 className="right">Amigo o enemigo</h4>
+            <p className="right">Pecados del pasado?</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            at laoreet arcu, a condimentum sapien. Orci varius natoque penatibus
+            et magnis dis parturient montes, nascetur ridiculus mus. Duis cursus
+            sapien ut diam laoreet porttitor. Interdum et malesuada fames ac
+            ante ipsum primis in faucibus</p>
         </section>
     )  
 };
